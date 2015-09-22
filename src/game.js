b4569165d21ab23db7ee525a5c7f74b71191ec8d
sherrypan <sherrypann@outlook.com>
2015-09-22 10:40:50 +0800

    modify game remove getFinalPoint

--- src/game.js
+++ src/game.js
@@ -2,34 +2,22 @@ var Frame = require("./frame.js");
 
 function Game(){
     this.frames = [];
-    this.bonus = {};
 }
 
 Game.prototype.scan = function(string){
 
-    var frames = string.split("|",10);
+    var frame = new Frame();
+    var frames = string.split("|", 10);
 
     for(var i = 0; i < frames.length; i ++){
         var balls =  frames[i].split('');
-        this.frames.push(new Frame(i+1, balls));
+
+        this.frames.push(frame.getBallsPoint(balls));
     }
 
-    this.bonus['balls'] = string.split('||')[1].split('');
-
+    var bonus = string.split('||')[1].split('');
+    this.frames.push(frame.getBallsPoint(bonus));
 
 }
-
-Game.prototype.getFinalPoint = function(game){
-
-    var point = 0;
-
-    game.frames.forEach(function(frame){
-        point += frame.getEachPoint(game)
-    });
-
-    return point;
-}
-
-
 
 module.exports = Game;
